fbd1612aeab6d78d22a2f309058480d5b10acfab
skeczeli <skeczeli@gmail.com>
2026-08-13 20:14:51 -0300

    refactor: modernize test.js with Mocha and Chai

--- corta/test/test.js
+++ corta/test/test.js
@@ -1,14 +1,35 @@
-// pruebas rapidas a mano - correr con el server levantado: node test.js
+const { expect } = require('chai');
 const axios = require('axios');
 
-axios.post('http://localhost:3000/api/links', { url: 'https://www.google.com' })
-  .then(function (r) {
-    console.log('creado:', r.data);
-    return axios.get('http://localhost:3000' + r.data.corta);
-  })
-  .then(function (r) {
-    console.log('redirect ok?', r.status);
+const BASE_URL = 'http://localhost:3000';
+
+describe('Corta', () => {
+  let codigo, corta;
+
+  describe('POST /api/links', () => {
+    it('crea un link corto', async () => {
+      const res = await axios.post(`${BASE_URL}/api/links`, {
+        url: 'https://www.google.com'
+      });
+      expect(res.status).to.equal(200);
+      expect(res.data).to.have.property('codigo');
+      expect(res.data).to.have.property('corta');
+
+      codigo = res.data.codigo;
+      corta = res.data.corta;
+    });
   });
 
-// TODO probar los clicks
-// TODO probar stats cuando este hecho el endpoint
+  describe('GET /:codigo', () => {
+    it('redirige al destino', async () => {
+      const res = await axios.get(`${BASE_URL}${corta}`, {
+        maxRedirects: 0,
+        validateStatus: () => true
+      });
+      expect(res.data).to.equal('https://www.google.com');
+    });
+  });
+
+  // TODO probar los clicks
+  // TODO probar stats cuando este hecho el endpoint
+});
